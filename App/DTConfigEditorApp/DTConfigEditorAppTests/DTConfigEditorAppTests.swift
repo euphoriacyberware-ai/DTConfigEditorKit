969406f689939694f009c04f07a5b76efcfff258
euphoriacyberware-ai <brian.cantin@gmail.com>
2026-08-02 09:31:21 -0400

    Adding app to project

---
+++ App/DTConfigEditorApp/DTConfigEditorAppTests/DTConfigEditorAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  DTConfigEditorAppTests.swift
+//  DTConfigEditorAppTests
+//
+//  Created by Brian Cantin on 2026-08-02.
+//
+
+import Testing
+
+struct DTConfigEditorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
